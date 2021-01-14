a1011d89b9fa1af2c6e1c4ab921453e847350f18
dini212 <dwirahmadini1234@gmail.com>
2021-01-14 18:10:27 +0700

    uas PBO

---
+++ checkLength.php
@@ -0,0 +1,13 @@
+<?php
+
+class checkLength extends index
+{
+    public static function check($sum)
+    {
+        if($wordInspiration.length() == 130) {
+            $sum = 3;
+        } else {
+            $sum = 0;
+        }
+    }    
+}
