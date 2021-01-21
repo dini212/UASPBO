4e70093f4aabff2365855c2e9dacf6c171bab25f
dini212 <dwirahmadini1234@gmail.com>
2021-01-21 11:07:31 +0700

    revisi2

--- main.php
+++ main.php
@@ -4,7 +4,7 @@ require_once __DIR__ . "/totalSum.php";
 require_once __DIR__ . "/checkLength.php";
 require_once __DIR__ . "/quantity.php";
 
-$inspiration1 = new wordInspiration("my world is your w@rld");
+$inspiration1 = new wordInspiration("HALo 212+-#ada@@dimana.!");
 $text = new totalSum();
 
 $total = $text->addCounts($inspiration1);
